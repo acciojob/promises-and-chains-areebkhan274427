6ec7e36141f50958cafdace248f6b2836c5c1251
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-07 20:44:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+var ageElement=document.getElementById('age');
+var nameElement=document.getElementById('name');
+
+
+
+var promise= new Promise((resolve,reject)=>{
+     setTimeout(()=>{
+		 
+        var age=ageElement.value;
+        var name=nameElement.value;
+		 
+       if(age<18){
+           reject(`Oh sorry ${name}. You aren't old enough.`);
+       }
+         else{
+             resolve(`Welcome, ${name}. You can vote.`);
+         }
+       
+   },4000);
+});
+
+function call(){
+    return promise;
+}
+
+
+function myFunction() {
+   call().then(function(data){
+       alert(data);
+   }).catch(function(data){
+       alert(data);
+   })
+}
+
+
